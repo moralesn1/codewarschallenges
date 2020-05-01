9a481b119b474fb6492d8d52511fc33682168e30
Nathaniel <nathanielmorales01@gmail.com>
2020-05-01 21:29:33 +1000

    find way to loop over elements and only capitalise first string value

--- 7kyu_accum.js
+++ 7kyu_accum.js
@@ -1,12 +1,29 @@
-function accum(s) {
-  let stringArray = s.split("");
-  for (i = 0; i < stringArray.length - 1; i++) {
-    stringArray[i] += stringArray[i];
-  }
-  return stringArray;
-}
+// function accum(s) {
+//   let stringArray = s.split("");
+//   let emptyArray = [];
+//   for (i = 0; i < stringArray.length; i++) {
+//     let newElement = stringArray[i].toString();
+//     emptyArray.push(newElement);
+//   }
+//   return emptyArray.map((item, index) => {
+//     return item.repeat(index + 1);
+//   });
+// }
 // return stringArray;
 
+function accum(s) {
+  let stringArray = s.split("");
+  let newArray = stringArray.map(valueToIndexRepeater);
+  return newArray;
+
+  // for (i in stringArray) {
+  //   return (i = stringArray[0].toUpperCase() + i.substr(1).toLowerCase());
+  // }
+}
+
+const valueToIndexRepeater = (item, index) => {
+  return item.repeat(index + 1);
+};
 console.log(accum("abcd"));
 //  -> "A-Bb-Ccc-Dddd"
 
