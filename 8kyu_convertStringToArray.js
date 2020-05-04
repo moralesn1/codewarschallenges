3e069b2e4459c5542c13a43072b0cace3b024954
Nathaniel <nathanielmorales01@gmail.com>
2020-05-04 16:33:00 +1000

    add new challenge

---
+++ 8kyu_convertStringToArray.js
@@ -0,0 +1,13 @@
+function stringToArray(string) {
+  return string.split(" ");
+}
+
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+console.log((stringToArray("Robin Singh"), ["Robin", "Singh"]));
+console.log(
+  (stringToArray("I love arrays they are my favorite"),
+  ["I", "love", "arrays", "they", "are", "my", "favorite"])
+);
