f0e10d391655a1f71641dbac543cd4deee781735
Nathaniel <nathanielmorales01@gmail.com>
2020-05-04 18:07:16 +1000

    new challenge

---
+++ 8kyu_reduceButGrow.js
@@ -0,0 +1,21 @@
+/*  Given a non-empty array of integers, return the result 
+of multiplying the values together in order. 
+
+Example: 
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+*/
+
+function grow(x) {
+  const total = x.reduce((acc, cur) => acc * cur);
+  return total;
+}
+
+console.log(grow([1, 2, 3]));
+// 6
+console.log(grow([4, 1, 1, 1, 4]));
+// 16
+console.log(grow([2, 2, 2, 2, 2, 2]));
+// 64
+console.log(grow(["a", 2, 2, 3, 2, 2, 2]));
