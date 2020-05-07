f68039fc08d06e19bcfb9b38942d0dbaa56025c7
Nathaniel <nathanielmorales01@gmail.com>
2020-05-07 20:59:09 +1000

    complete challenge

---
+++ 7kyu/7kyu_summingNumbersdigits.js
@@ -0,0 +1,16 @@
+function sumDigits(number) {
+  const numberArray = Array.from(String(number), Number).filter((a) => {
+    if (a == "-") {
+      return;
+    } else {
+      return a;
+    }
+  });
+
+  return numberArray.reduce((a, b) => a + b, 0);
+}
+
+console.log(sumDigits(10), 1);
+console.log(sumDigits(99), 18);
+console.log(sumDigits(-32), 5);
+console.log(sumDigits(0), 0);
