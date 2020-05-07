e0e8cab73d4d54af5c79c7e808f7b45e0fbb171a
Nathaniel <nathanielmorales01@gmail.com>
2020-05-07 21:01:41 +1000

    add note

--- 7kyu/7kyu_summingNumbersdigits.js
+++ 7kyu/7kyu_summingNumbersdigits.js
@@ -1,16 +1,18 @@
 function sumDigits(number) {
-  const numberArray = Array.from(String(number), Number).filter((a) => {
-    if (a == "-") {
-      return;
-    } else {
-      return a;
-    }
-  });
-
-  return numberArray.reduce((a, b) => a + b, 0);
+  // const numberArray = Array.from(String(number), Number).filter((a) => {
+  //   if (a == "-") {
+  //     return;
+  //   } else {
+  //     return a;
+  //   }
+  // });
+  // return numberArray.reduce((a, b) => a + b, 0);
 }
 
 console.log(sumDigits(10), 1);
 console.log(sumDigits(99), 18);
 console.log(sumDigits(-32), 5);
 console.log(sumDigits(0), 0);
+
+// TOP SOLUTION
+// return Math.abs(number).toString().split('').reduce((a,b) => +a + +b, 0)
