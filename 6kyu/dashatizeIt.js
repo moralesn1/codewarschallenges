e1ce656332043987c60aa9490fa6b880f26391c1
Nathaniel <nathanielmorales01@gmail.com>
2020-09-07 14:39:22 +1000

    add code challenge

---
+++ 6kyu/dashatizeIt.js
@@ -0,0 +1,57 @@
+// Given a number, return a string with dash'-'marks before and after each odd integer,
+// but do not begin or end the string with a dash mark.
+
+// Ex:
+
+// dashatize(274) -> '2-7-4'
+// dashatize(6815) -> '68-1-5'
+
+function dashatize(num) {
+  if (isNaN(num)) {
+    return "NaN";
+  }
+  if (num.length === 1) {
+    console.log("hit");
+    return num.join("").replace(/-/g, "");
+  }
+
+  let array = num.toString().split("");
+  console.log(num);
+
+  let newArray = array.map((x) => {
+    return parseInt(x, 10);
+  });
+
+  let dashed2 = newArray.map((current, index, array) => {
+    if (isNaN(current)) {
+      current = "";
+    }
+
+    if (current % 2 == 0) {
+      return current;
+    }
+    if (index !== 0 && index !== array.length - 1 && current % 2 !== 0) {
+      return "-" + current + "-";
+    }
+    if (index == 0 && current % 2 !== 0) {
+      return current + "-";
+    } else if (array.length - 1 && current % 2 !== 0) {
+      return "-" + current;
+    }
+  });
+
+  return dashed2
+    .join(",")
+    .replace(/,/g, "")
+    .replace(/--/g, "-")
+    .replace(/(^-|-$)/g, "");
+}
+
+// Top Solution
+
+function dashatize(num) {
+  return String(num)
+    .replace(/([13579])/g, "-$1-")
+    .replace(/--+/g, "-")
+    .replace(/(^-|-$)/g, "");
+}
---
+++ 6kyu/formatAStringOfNames.js
@@ -0,0 +1,92 @@
+// Return: a string formatted as a list of names separated by commas except for the last two names,
+// which should be separated by an ampersand.
+
+// Example:
+
+// list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
+// // returns 'Bart, Lisa & Maggie'
+
+// list([ {name: 'Bart'}, {name: 'Lisa'} ])
+// // returns 'Bart & Lisa'
+
+// list([ {name: 'Bart'} ])
+// // returns 'Bart'
+
+// list([])
+// // returns ''
+
+function list(names) {
+  let listOfNames = [];
+
+  let part1 = [];
+  let part2 = [];
+
+  names.forEach((element, index, arr) => {
+    listOfNames.push(element.name);
+  });
+
+  if (listOfNames.length === 2) {
+    listOfNames.splice(-1, 0, "&");
+    return listOfNames.join(" ");
+  }
+
+  if (listOfNames.length === 3) {
+    part1 = listOfNames.slice(0, -2).join(", ");
+    part2 = listOfNames.slice(-2);
+    part2.splice(-1, 0, "&").join(", ");
+    let p2 = ", " + part2.join(" ");
+    return part1 + p2;
+  }
+
+  if (listOfNames.length >= 4) {
+    part1 = listOfNames.slice(0, -2).join(", ");
+    part2 = listOfNames.slice(-2);
+    part2.splice(-1, 0, "&").join(", ");
+    let p2 = ", " + part2.join(" ");
+    return part1 + p2;
+  }
+
+  return listOfNames.join(" ");
+}
+
+Test.assertEquals(
+  list([
+    { name: "Bart" },
+    { name: "Lisa" },
+    { name: "Maggie" },
+    { name: "Homer" },
+    { name: "Marge" },
+  ]),
+  "Bart, Lisa, Maggie, Homer & Marge",
+  "Must work with many names"
+);
+Test.assertEquals(
+  list([{ name: "Bart" }, { name: "Lisa" }, { name: "Maggie" }]),
+  "Bart, Lisa & Maggie",
+  "Must work with many names"
+);
+Test.assertEquals(
+  list([{ name: "Bart" }, { name: "Lisa" }]),
+  "Bart & Lisa",
+  "Must work with two names"
+);
+Test.assertEquals(
+  list([{ name: "Bart" }]),
+  "Bart",
+  "Wrong output for a single name"
+);
+Test.assertEquals(list([]), "", "Must work with no names");
+
+// TOP SOLUTION
+
+function list(names) {
+  return names.reduce(function (prev, current, index, array) {
+    if (index === 0) {
+      return current.name;
+    } else if (index === array.length - 1) {
+      return prev + " & " + current.name;
+    } else {
+      return prev + ", " + current.name;
+    }
+  }, "");
+}
