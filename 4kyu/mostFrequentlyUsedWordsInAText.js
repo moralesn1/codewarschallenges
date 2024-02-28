04cee40e7d356c2d50a8fcf199ec2d5b5ad4ae53
unknown <nathanielmorales01@gmail.com>
2024-02-28 14:14:49 +1100

    add folder

---
+++ 4kyu/mostFrequentlyUsedWordsInAText.js
@@ -0,0 +1,14 @@
+// Write a function that, given a string of text (possibly with punctuation and line-breaks), returns an array of the 
+// top-3 most occurring words, in descending order of the number of occurrences.
+// Assumptions:
+
+//     A word is a string of letters (A to Z) optionally containing one or more apostrophes (') in ASCII.
+//     Apostrophes can appear at the start, middle or end of a word ('abc, abc', 'abc', ab'c are all valid)
+//     Any other characters (e.g. #, \, / , . ...) are not part of a word and should be treated as whitespace.
+//     Matches should be case-insensitive, and the words in the result should be lowercased.
+//     Ties may be broken arbitrarily.
+//     If a text contains fewer than three unique words, then either the top-2 or top-1 words should be returned, 
+//     or an empty array if a text contains no words.
+
+// https://www.codewars.com/kata/51e056fe544cf36c410000fb/train/javascript
+
